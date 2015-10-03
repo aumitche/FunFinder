5d80e3584eeb641b156296908e89fa9365dfd2f1
Farhad Yusufali <farhad.yusufali@icloud.com>
2015-10-03 01:11:30 -0400

    Working on receiving data and sending to server

--- FunTracker/FunTracker/AppDelegate.swift
+++ FunTracker/FunTracker/AppDelegate.swift
@@ -47,7 +47,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, ESTBeaconManagerDelegate 
 	func applicationWillTerminate(application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 	}
-
-
 }
 
